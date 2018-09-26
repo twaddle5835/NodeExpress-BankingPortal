9f3178aa6690c7788ae5453a00e7cafdf7f6c120
Tim Waddle <twaddle@daytonfreight.com>
2018-09-26 15:46:39 -0400

    App.js: read files for accounts and users, change index to summary page, add pages for different accounts and profile. Account.ejs show account transactions.  Create profile.ejs, show user information.

--- src/app.js
+++ src/app.js
@@ -1,15 +1,78 @@
+//File stream
 const fs = require('fs');
+
+//Path
 const path = require('path');
 
+//Express
 const express = require('express');
-
 const app = express();
 
+//Set path to views folder
 app.set('views', path.join(__dirname, 'views'));
+
+//View engine
 app.set('view engine', 'ejs');
 
+//CSS
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => res.render('index', { title: 'Index' }));
+//Read account data
+const accountData = fs.readFileSync(
+    path.join(__dirname, 'json', 'accounts.json'), 'utf8'
+);
 
-app.listen(3000, () => console.log('PS Project Running on port 3000!'));
+const accounts = JSON.parse(accountData);
+
+//Read user data
+const userData = fs.readFileSync(
+    path.join(__dirname, 'json', 'users.json'), 'utf8'
+);
+
+const users = JSON.parse(userData);
+
+//index root
+app.get('/', (req, res) => {
+    res.render('index',
+        {
+            title: 'Account Summary',
+            accounts: 'accounts'
+        });
+});
+
+//savings
+app.get('/savings', (req, res) => {
+    res.render('account',
+        {
+            account: 'accounts.savings'
+        });
+});
+
+//checking
+app.get('/savings', (req, res) => {
+    res.render('checking',
+        {
+            account: 'accounts.checking'
+        });
+});
+
+//credit
+app.get('/savings', (req, res) => {
+    res.render('credit',
+        {
+            account: 'accounts.credit'
+        });
+});
+
+//Profile
+app.get('/profile', (req, res) => {
+    res.render('profile',
+    {
+        user: user[0] //First user in the users.json
+    });
+});
+
+//Port 3000
+app.listen(3000, () => {
+    console.log('PS Project Running on port 3000!');
+});
