9dc1ffbf434d76c1180373fcfeb8fe478ec22248
Tim Waddle <twaddle@daytonfreight.com>
2018-09-26 14:54:11 -0400

    Fix app.set issue

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 const fs = require('fs');
 const path = require('path');
+
 const express = require('express');
-const app = express;
+
+const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
