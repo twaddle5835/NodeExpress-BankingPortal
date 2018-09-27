90d24f33540a140caeebb32d52e83593d162aabf
Tim Waddle <TWaddle@daytonfreight.com>
2018-09-27 16:28:13 -0400

    App.js cleanup

--- src/app.js
+++ src/app.js
@@ -19,51 +19,40 @@ const accountRoutes = require('./routes/accounts');
 const servicesRoutes = require('./routes/services');
 
 //Set path to views folder
-app.set(
-    'views',
-    path.join(
-        __dirname, 'views'
-    )
+app.set('views',
+    path.join(__dirname, 'views')
 );
 
 //View engine
-app.set(
-    'view engine', 'ejs'
-);
+app.set('view engine', 'ejs');
 
 //CSS
 app.use(
     express.static(
-        path.join(
-            __dirname, 'public'
-        )
+        path.join(__dirname, 'public')
     )
-);
+); //CSS
 
 //URL Encoded Middleware
 app.use(
     express.urlencoded({
         extended: true
     })
-);
+); //URL Encoded Middleware
 
 //index root
 app.get('/', (req, res) => {
-    res.render(
-        'index', {
-            title: 'Account Summary', accounts
-        }
-    );
-});
+    res.render('index', {
+        title: 'Account Summary', accounts
+    });
+}); //index root
 
 //Profile
 app.get('/profile', (req, res) => {
-    res.render(
-        'profile', {
-            user: users[0] //First user in the users.json
-        }
-    );
-});
+    res.render('profile', {
+        user: users[0] //First user in the users.json
+    });
+}); //Profile
 
 //Accounts
 app.use('/account', accountRoutes);
@@ -72,8 +61,6 @@ app.use('/account', accountRoutes);
 app.use('/services', servicesRoutes);
 
 //Port 3000
-app.listen(
-    3000, () => {
-        console.log('PS Project Running on port 3000!');
-    }
-);
+app.listen(3000, () => {
+    console.log('PS Project Running on port 3000!');
+}); //Port 3000
